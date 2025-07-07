8d0a52b7b12c3afefd7c07f2c34d49791e323e81
rahi2109 <rahi.anshuman2109@gmail.com>
2025-07-07 07:53:11 +0530

    added user api

---
+++ blog_app_backend/src/main/java/com/example/blog_app/model/Category.java
@@ -0,0 +1,11 @@
+package com.example.blog_app.model;
+
+import java.util.List;
+
+public class Category {
+	Integer CategoryId;
+	String categoryTitle;
+	
+	List<Post> posts;
+
+}
---
+++ blog_app_backend/src/main/java/com/example/blog_app/model/Comment.java
@@ -0,0 +1,10 @@
+package com.example.blog_app.model;
+
+public class Comment {
+	Integer commentId;
+	String comment;
+	
+	User user;
+	Post post;
+
+}
